6de9b990e0e7f7f6b0546715fb2388b6acb221ea
“Laura <114937668+L-itslocked@users.noreply.github.com>
2023-06-04 14:39:05 -0400

    Fix email validation and add else if

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ function btnHandler() {
   let lastname = document.getElementById("name-last").value;
   let email = document.getElementById("email").value;
   let pw = document.getElementById("password").value;
+  let char = /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/;
 
   if (firstname.length < 1) {
     document.getElementById("invalid-first").innerHTML =
@@ -16,12 +17,15 @@ function btnHandler() {
     document.getElementById("invalid-last").innerHTML =
       "Last Name cannot be empty";
   }
-  if (email.length < 1) {
+  if (email.length < 1 || char.test(email) == false) {
     document.getElementById("invalid-email").innerHTML =
       "Looks like this is not an email";
   }
-  if (pw.length <= 1) {
+
+  if (pw.length == 0) {
     document.getElementById("invalid-pw").innerHTML =
       "Password cannot be empty";
+  } else if (pw.length < 15) {
+    document.getElementById("invalid-pw").innerHTML = "Password must be a minimum of 15 characters"
   }
 }
