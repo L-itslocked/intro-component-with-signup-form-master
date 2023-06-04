ea52e7de3b8d814a7c780f23d6720c5fd808e524
“Laura <114937668+L-itslocked@users.noreply.github.com>
2023-06-04 14:19:53 -0400

    Add if arguments

--- script.js
+++ script.js
@@ -2,29 +2,26 @@ let btn = document.getElementById("trial-btn");
 
 btn.addEventListener("click", btnHandler);
 
-
-
-
-
 function btnHandler() {
-  console.log("clicked");
   let firstname = document.getElementById("name-first").value;
   let lastname = document.getElementById("name-last").value;
   let email = document.getElementById("email").value;
   let pw = document.getElementById("password").value;
 
-  if (firstname == "") {
-    console.log("All fields must be filled out");
+  if (firstname.length < 1) {
+    document.getElementById("invalid-first").innerHTML =
+      "First Name cannot be empty";
   }
-  if (lastname == "") {
-    console.log("Last Name must be filled out");
+  if (lastname.length < 1) {
+    document.getElementById("invalid-last").innerHTML =
+      "Last Name cannot be empty";
   }
-  if (email == "") {
-    console.log("invalid email");
+  if (email.length < 1) {
+    document.getElementById("invalid-email").innerHTML =
+      "Looks like this is not an email";
   }
-  if (pw == "") {
-    console.log("Please create a password");
-  } else {
-    console.log("form has been completed");
+  if (pw.length <= 1) {
+    document.getElementById("invalid-pw").innerHTML =
+      "Password cannot be empty";
   }
 }
