87505d8378da365b072103a5a175f0eb9a18cf8e
“Laura <114937668+L-itslocked@users.noreply.github.com>
2023-06-04 10:49:09 -0400

    Add btnHandler function

--- script.js
+++ script.js
@@ -2,4 +2,29 @@ let btn = document.getElementById("trial-btn");
 
 btn.addEventListener("click", btnHandler);
 
-function btnHandler() {}
+
+
+
+
+function btnHandler() {
+  console.log("clicked");
+  let firstname = document.getElementById("name-first").value;
+  let lastname = document.getElementById("name-last").value;
+  let email = document.getElementById("email").value;
+  let pw = document.getElementById("password").value;
+
+  if (firstname == "") {
+    console.log("All fields must be filled out");
+  }
+  if (lastname == "") {
+    console.log("Last Name must be filled out");
+  }
+  if (email == "") {
+    console.log("invalid email");
+  }
+  if (pw == "") {
+    console.log("Please create a password");
+  } else {
+    console.log("form has been completed");
+  }
+}
